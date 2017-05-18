e9ae8c49b674709ccff2af3070ea27bf47d4d0fc
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-05-18 01:03:26 -0700

    make it better

---
+++ app/index.js
@@ -0,0 +1,54 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
+
+class Users extends React.Component {
+  render() {
+    var friends = this.props.list.filter(function (user) {
+            return user.friend === true
+          }) 
+    
+    var nonFriends = this.props.list.filter(function (user) {
+            return user.friend !== true
+          })
+  
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function (user) {
+            return <li key={user.name}>{user.name}</li>
+          })}
+        </ul>
+        
+        <hr />
+        
+        <h1> Non Friends </h1>
+        <ul>
+          {nonFriends.map(function (user) {
+            return <li key={user.name}>{user.name}</li>
+          })}
+        </ul>        
+      </div>
+    )
+  }
+}
+
+Users.propTypes = {
+	list: PropTypes.arrayOf(PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		friend: PropTypes.bool.isRequired,
+	})),
+}
+
+ReactDOM.render(
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]} 
+  />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var config = {
+	entry: './app/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bundle.js'
+	},
+	module: {
+		rules: [
+			{ test: /\.(js)$/, use: 'babel-loader'},
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+		]
+	},
+	devServer: {
+		historyAPIFallback: true,
+	},
+	plugins: [new HtmlWebpackPlugin({
+		template: 'app/index.html'
+	})]
+}
+
+if (process.env.NODE_ENV === 'production') {
+	config.plugins.push(
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	)
+}
+
+module.exports = config
