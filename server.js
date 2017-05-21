b235c5207c88455a14ebf9bb7cd5490e81ccf297
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-05-21 03:48:51 -0700

    Add . in front of /dist

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 
 // Create our app
 var app = express();
@@ -7,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static((__dirname + '/dist')));
 
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, '/dist', 'index.html'))
+  response.sendFile(path.resolve(__dirname, './dist', 'index.html'))
 })
 
 app.listen(PORT, function () {
