a4205b01db3cfef76bfa4a96aeddfe47bd9956fc
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-05-21 02:31:13 -0700

    Add router

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,24 @@
 var React = require('react');
 var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className='container'>
-				<Popular />
-			</div>
+			<Router>
+				<div className='container'>
+					<Nav />
+					<Route exact path='/' component={Home} />
+					<Route path='/battle' component={Battle} />
+					<Route path='/popular' component={Popular} />
+				</div>
+			</Router>
+			
 		);
 	}
 }
